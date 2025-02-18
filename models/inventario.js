76ce48406a650acc69240de9ba1481f1bde23b14
IN5TAKILL538 <galviscristian538@gmail.com>
2025-02-18 10:00:41 -0500

    modelos inventario y productos

---
+++ models/inventario.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const InventarioSchema = new mongoose.Schema({
+    productoId: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Productos' },// Ref a la colección Productos, si la tienes
+    type: {type: String, required: true, enum: ['entrada', 'salida', 'devolucion']},
+    quantity: {type: Number, required: true},
+    date: {type: Date, default: Date.now},
+    userId: {type: mongoose.Schema.Types.ObjectId, required: true,ref: 'Usuarios' },// Ref a la colección Usuarios, si la tienes
+    reason: {type: String, default: 'Compra de proveedor:'},
+
+}, { timestamps: true });
+
+// Crear el modelo
+const Inventario = mongoose.model('Inventario', InventarioSchema);
+
+module.exports = Inventario;
